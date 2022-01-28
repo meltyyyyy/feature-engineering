7decd6e6be695999ecbb4f91b0f216042957647f
takeru.abe <takeru.abe@gaiax.com>
2022-01-28 11:42:55 +0900

    regression for binned data

--- binning/binning.py
+++ binning/binning.py
@@ -9,10 +9,12 @@ from sklearn.tree import DecisionTreeRegressor
 class Binning:
     X, y = mglearn.datasets.make_wave(n_samples=100)
     line = np.linspace(-3, 3, 1000, endpoint=False).reshape(-1, 1)
+    bins = np.linspace(-3, 3, 11)
 
     def main(self):
         self.showFigure()
         self.execute()
+        self.regression_with_binned_data()
 
     def showFigure(self):
         fig = plt.figure()
@@ -30,10 +32,9 @@ class Binning:
         fig.savefig('binning/regressions.png')
 
     def execute(self):
-        bins = np.linspace(-3, 3, 11)
-        print("bins: {}".format(bins))
+        print("bins: {}".format(self.bins))
 
-        which_bin = np.digitize(self.X, bins=bins)
+        which_bin = np.digitize(self.X, bins=self.bins)
         print("\nData points:\n", self.X[:5])
         print("\nBin membership for data points:\n", which_bin[:5])
 
@@ -44,4 +45,23 @@ class Binning:
 
         print("X_binned.shape: {}".format(X_binned.shape))
 
+    def regression_with_binned_data(self):
+        which_bin = np.digitize(self.X, bins=self.bins)
+        encoder = OneHotEncoder(sparse=False)
+        encoder.fit(which_bin)
+        X_binned = encoder.transform(which_bin)
+        line_binned = encoder.transform(np.digitize(self.line,bins=self.bins))
 
+        fig = plt.figure()
+
+        reg = LinearRegression().fit(X_binned,self.y)
+        plt.plot(self.line,reg.predict(line_binned),label="linear regression binned")
+
+        reg = DecisionTreeRegressor(min_samples_split=3).fit(X_binned,self.y)
+        plt.plot(self.line,reg.predict(line_binned),label="decision tree binned")
+        plt.plot(self.X[:,0],self.y,'o',c='k')
+        plt.vlines(self.bins,-3,3,linewidth = 1,alpha=.2)
+        plt.legend(loc="best")
+        plt.ylabel("Regression input")
+        plt.xlabel("Input feature")
+        fig.savefig('binning/binned_data_regression.png')
