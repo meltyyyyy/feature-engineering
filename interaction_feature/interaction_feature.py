c3412b0b91d3f94a2aca168c2836124bb236c31a
takeru.abe <takeru.abe@gaiax.com>
2022-01-29 13:17:37 +0900

    add product

--- interaction_feature/interaction_feature.py
+++ interaction_feature/interaction_feature.py
@@ -15,12 +15,14 @@ class InteractionFeature:
     X_binned = encoder.transform(which_bin)
     line_binned = encoder.transform(np.digitize(line, bins=bins))
     X_combined = np.hstack([X, X_binned])
+    X_product = np.hstack([X_binned,X*X_binned])
     print(X_combined.shape)
 
     def main(self):
-        self.linear_regression()
+        self.bin_combined()
+        self.bin_product()
 
-    def linear_regression(self):
+    def bin_combined(self):
         reg = LinearRegression().fit(self.X_combined, self.y)
 
         line_combined = np.hstack([self.line, self.line_binned])
@@ -34,3 +36,18 @@ class InteractionFeature:
         plt.xlabel("Input feature")
         plt.plot(self.X[:, 0], self.y, 'o', c='k')
         fig.savefig('interaction_feature/bin_combined.png')
+
+    def bin_product(self):
+        reg = LinearRegression().fit(self.X_product, self.y)
+
+        line_product = np.hstack([self.line_binned, self.line * self.line_binned])
+        fig = plt.figure()
+        plt.plot(self.line, reg.predict(line_product), label='linear regression combined')
+
+        for bin in self.bins:
+            plt.plot([bin, bin], [-3, 3], ':', c='k')
+        plt.legend(loc="best")
+        plt.ylabel("Regression output")
+        plt.xlabel("Input feature")
+        plt.plot(self.X[:, 0], self.y, 'o', c='k')
+        fig.savefig('interaction_feature/bin_product.png')
