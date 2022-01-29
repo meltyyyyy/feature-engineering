0d46d26ff49e4d38de79fcfb05a468287819224b
takeru.abe <takeru.abe@gaiax.com>
2022-01-29 12:08:43 +0900

    update main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+import sys
+
 from binning.binning import Binning
 from one_hot_encoding.one_hot_encoding import OneHotEncoding
 
 if __name__ == '__main__':
-    # one_hot_encoding = OneHotEncoding()
-    # one_hot_encoding.main()
-    binning = Binning()
-    binning.main()
+    if sys.argv[1] == 'one_hot_encoding':
+        one_hot_encoding = OneHotEncoding()
+        one_hot_encoding.main()
+    elif sys.argv[1] == 'binning':
+        binning = Binning()
+        binning.main()
+    else:
+        print('Invalid augment')
